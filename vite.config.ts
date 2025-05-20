0d33bfb49778375ef820f7ed736af9d536ea0d3e
emipignata <emipignata@gmail.com>
2025-05-19 23:45:32 -0300

    raiz ok

--- vite.config.ts
+++ vite.config.ts
@@ -19,5 +19,6 @@ export default defineConfig(({ mode }) => ({
       "@": path.resolve(__dirname, "./src"),
     },
   },
-  base: "/lares-aislaciones-barcelona/"
+  //base: "/lares-aislaciones-barcelona/"
+  base: "./"
 }));
